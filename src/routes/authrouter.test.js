9a0c4fdbbd20659dd4b2a80e9e1c69c568f8d34e
Isaac Smith <isaacwallacesmith@gmail.com>
2026-09-23 08:40:27 -0600

    auth tests

--- src/routes/authrouter.test.js
+++ src/routes/authrouter.test.js
@@ -54,3 +54,47 @@ test('admin login', async () => {
   delete expectedUser.password;
   expect(loginRes.body.user).toMatchObject(expectedUser);
 });
+
+test('create user', async () => {
+  const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const expectedUser = { ...user, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(registerRes.body.user).toMatchObject(expectedUser);
+});
+
+test('update user', async () => {
+  const user = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const updateRes = await request(app).put('/api/auth').send(user);
+  expect(updateRes.status).toBe(200);
+  expectValidJwt(updateRes.body.token);
+
+  const expectedUser = { ...user, roles: [{ role: 'diner' }] };
+  delete expectedUser.password;
+  expect(registerRes.body.user).toMatchObject(expectedUser);
+});
+
+test('logout', async () => {
+  const user = { name: 'pizza diner', email: randomName() + '@test.com', password: 'a' };
+  const registerRes = await request(app).post('/api/auth').send(user);
+  expect(registerRes.status).toBe(200);
+  expectValidJwt(registerRes.body.token);
+
+  const logoutRes = await request(app).delete('/api/auth').set('Authorization', `Bearer ${registerRes.body.token}`);
+  expect(logoutRes.status).toBe(200);
+  expect(logoutRes.body.message).toBe('logout successful');
+});
+
+test('logout without a token is unauthorized', async () => {
+  const logoutRes = await request(app).delete('/api/auth');
+  expect(logoutRes.status).toBe(401);
+  expect(logoutRes.body.message).toBe('unauthorized');
+});
+
